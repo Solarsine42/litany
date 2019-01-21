876306362a9e459f3e1bab6f3e1828b86aa4dfa3
Solarsine42 <46771803+Solarsine42@users.noreply.github.com>
2019-01-20 23:03:17 -0700

    update3

--- LitanyProject/litany.js
+++
@@ -1,6 +0,0 @@
-function check(){
-  document.getElementById('f1').innerHTML=document.iteration.focus.value;
-  document.getElementById('f2').innerHTML=document.iteration.type.value;
-  document.getElementById('f3').innerHTML=document.iteration.form.value;
-  document.getElementById('f4').innerHTML=document.iteration.strength.value;
-}
